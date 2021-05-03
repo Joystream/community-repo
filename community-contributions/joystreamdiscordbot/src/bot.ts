2c5be78229efd2305688bbb9bc24a7d72d968bd8
Joystream Stats <dev@joystreamstats.live>
2021-05-03 22:45:26 +0000

    add dependencies

--- community-contributions/joystreamdiscordbot/src/bot.ts
+++ community-contributions/joystreamdiscordbot/src/bot.ts
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const { token } = require("./config.ts");
+const { token } = require("../config.ts");
 
 client.login(token);
 
