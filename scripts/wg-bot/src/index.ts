b313d201c0a55d7c135a560c579b93069b9c4b57
singulart <serpian@gmail.com>
2022-01-26 18:15:08 +0200

    Olympia-related dependencies bump

--- scripts/wg-bot/src/index.ts
+++ scripts/wg-bot/src/index.ts
@@ -1,6 +1,11 @@
 import { connectUpstream } from './joystream/ws'
 import { ApiPromise } from '@polkadot/api'
 import { BlockHash, EventRecord, Header } from '@polkadot/types/interfaces'
+import { wgEvents, workingGroups } from './config'
+import { AugmentedEvents } from '@polkadot/api/types/events'
+
+const TYPES_AVAILABLE = [] as const
+type ApiType = typeof TYPES_AVAILABLE[number]
 
 ;(async () => {
   
@@ -11,8 +16,11 @@ import { BlockHash, EventRecord, Header } from '@polkadot/types/interfaces'
           const blockEvents = await api.query.system.events.at(hash)
           blockEvents.forEach(async ({ event }: EventRecord) => {
             let { section, method, data } = event
-            console.log(section);
-            console.log(method);
+            if (wgEvents.includes(method) && Object.keys(workingGroups).includes(section)) {
+                console.log(section);
+                console.log(method);
+                console.log(data);
+            }
           });
         })  
       })
