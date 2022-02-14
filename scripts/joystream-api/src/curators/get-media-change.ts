60d7925235cb0b70eb2e9e069c915e83c17d9178
Joystream Stats <dev@joystreamstats.live>
2022-02-14 17:41:52 +0100

    joystream-api: prettier

--- scripts/joystream-api/src/curators/get-media-change.ts
+++ scripts/joystream-api/src/curators/get-media-change.ts
@@ -1,62 +1,96 @@
 import { WsProvider, ApiPromise } from "@polkadot/api";
 import { types } from "@joystream/types";
-import {  Vec } from "@polkadot/types";
+import { Vec } from "@polkadot/types";
 import { EventRecord, Hash } from "@polkadot/types/interfaces";
 import { getChangeAction } from "./functions";
 
-
 async function main() {
-    // Initialise the provider to connect to the local node
-    const provider = new WsProvider('ws://127.0.0.1:9944');
+  // Initialise the provider to connect to the local node
+  const provider = new WsProvider("ws://127.0.0.1:9944");
 
-    const api = await ApiPromise.create({ provider, types })
-    const firstBlock = 1292265 // first block after the upgrade to the new Content Directory
-    const lastBlock = 2332000
-    // note that with this blockheight, you will see a lot of jsgenesis initialization and uploads
-    
-    
-    for (let blockHeight=firstBlock; blockHeight<lastBlock; blockHeight++) {
-      const blockHash = await api.rpc.chain.getBlockHash(blockHeight) as Hash
-      const events = await api.query.system.events.at(blockHash) as Vec<EventRecord>;
-      const eventsArray: EventRecord[] = []
-      let eventIndex = 0
-      for (let i=0; i<events.length; i++) {
-        const section = events[i].event.section
-        const method = events[i].event.method
-        if(section == 'content') {
-          console.log(`Event section=${section}, Event method=${method}`)
-          eventsArray.push(events[i])
-          if (method == "VideoCreated") {
-            eventIndex+=1
-            const cdChange = await getChangeAction(api, 'createVideo', blockHeight, blockHash, eventIndex, events[i])
-            console.log("Change",JSON.stringify(cdChange, null, 4))
-          }
-          if (method == "VideoUpdated") {
-            eventIndex+=1
-            const cdChange = await getChangeAction(api, 'updateVideo', blockHeight, blockHash, eventIndex, events[i])
-            console.log("Change",JSON.stringify(cdChange, null, 4))
-          }
-          if (method == "VideoDeleted") {
-            eventIndex+=1
-            const cdChange = await getChangeAction(api, 'deleteVideo', blockHeight, blockHash, eventIndex, events[i])
-            console.log("Change",JSON.stringify(cdChange, null, 4))
-          }
-          if (method == "ChannelCreated") {
-            eventIndex+=1
-            const cdChange = await getChangeAction(api, 'createChannel', blockHeight, blockHash, eventIndex, events[i])
-            console.log("Change",JSON.stringify(cdChange, null, 4))
-          }
-          if (method == "ChannelUpdated") {
-            eventIndex+=1
-            const cdChange = await getChangeAction(api, 'updateChannel', blockHeight, blockHash, eventIndex, events[i])
-            console.log("Change",JSON.stringify(cdChange, null, 4))
-          }
+  const api = await ApiPromise.create({ provider, types });
+  const firstBlock = 1292265; // first block after the upgrade to the new Content Directory
+  const lastBlock = 2332000;
+  // note that with this blockheight, you will see a lot of jsgenesis initialization and uploads
+
+  for (let blockHeight = firstBlock; blockHeight < lastBlock; blockHeight++) {
+    const blockHash = (await api.rpc.chain.getBlockHash(blockHeight)) as Hash;
+    const events = (await api.query.system.events.at(
+      blockHash
+    )) as Vec<EventRecord>;
+    const eventsArray: EventRecord[] = [];
+    let eventIndex = 0;
+    for (let i = 0; i < events.length; i++) {
+      const section = events[i].event.section;
+      const method = events[i].event.method;
+      if (section == "content") {
+        console.log(`Event section=${section}, Event method=${method}`);
+        eventsArray.push(events[i]);
+        if (method == "VideoCreated") {
+          eventIndex += 1;
+          const cdChange = await getChangeAction(
+            api,
+            "createVideo",
+            blockHeight,
+            blockHash,
+            eventIndex,
+            events[i]
+          );
+          console.log("Change", JSON.stringify(cdChange, null, 4));
+        }
+        if (method == "VideoUpdated") {
+          eventIndex += 1;
+          const cdChange = await getChangeAction(
+            api,
+            "updateVideo",
+            blockHeight,
+            blockHash,
+            eventIndex,
+            events[i]
+          );
+          console.log("Change", JSON.stringify(cdChange, null, 4));
+        }
+        if (method == "VideoDeleted") {
+          eventIndex += 1;
+          const cdChange = await getChangeAction(
+            api,
+            "deleteVideo",
+            blockHeight,
+            blockHash,
+            eventIndex,
+            events[i]
+          );
+          console.log("Change", JSON.stringify(cdChange, null, 4));
+        }
+        if (method == "ChannelCreated") {
+          eventIndex += 1;
+          const cdChange = await getChangeAction(
+            api,
+            "createChannel",
+            blockHeight,
+            blockHash,
+            eventIndex,
+            events[i]
+          );
+          console.log("Change", JSON.stringify(cdChange, null, 4));
+        }
+        if (method == "ChannelUpdated") {
+          eventIndex += 1;
+          const cdChange = await getChangeAction(
+            api,
+            "updateChannel",
+            blockHeight,
+            blockHash,
+            eventIndex,
+            events[i]
+          );
+          console.log("Change", JSON.stringify(cdChange, null, 4));
         }
       }
     }
-    
-  api.disconnect()
+  }
+
+  api.disconnect();
 }
 
-main()
-
+main();
--- scripts/joystream-api/src/examples/general.ts
+++ scripts/joystream-api/src/examples/general.ts
@@ -1,69 +1,88 @@
 import { WsProvider, ApiPromise } from "@polkadot/api";
 import { types } from "@joystream/types";
 import { Vec } from "@polkadot/types";
-import { Balance, EventRecord, Extrinsic, SignedBlock } from "@polkadot/types/interfaces";
+import {
+  Balance,
+  EventRecord,
+  Extrinsic,
+  SignedBlock,
+} from "@polkadot/types/interfaces";
 
 async function main() {
-    // Initialise the provider to connect to the local node
-    const provider = new WsProvider('ws://127.0.0.1:9944');
-      
-    //If you want to play around on our staging network, go ahead and connect to this staging network instead.
-    //const provider = new WsProvider('wss://testnet-rpc-2-singapore.joystream.org');
+  // Initialise the provider to connect to the local node
+  const provider = new WsProvider("ws://127.0.0.1:9944");
 
-    // Create the API and wait until ready
-    const api = await ApiPromise.create({ provider, types })
+  //If you want to play around on our staging network, go ahead and connect to this staging network instead.
+  //const provider = new WsProvider('wss://testnet-rpc-2-singapore.joystream.org');
 
-    // get finalized head of chain, as number and hash:
-    const finalizedHeadNumber = await api.derive.chain.bestNumberFinalized()
-    const finalizedHeadHash = await api.rpc.chain.getFinalizedHead()
-    // get head of chain, as hash or number:
-    const headNumber = await api.derive.chain.bestNumber()
-    const headHash = await api.rpc.chain.getBlockHash(headNumber)
+  // Create the API and wait until ready
+  const api = await ApiPromise.create({ provider, types });
 
-    console.log(
-      `Finalized head of chain
+  // get finalized head of chain, as number and hash:
+  const finalizedHeadNumber = await api.derive.chain.bestNumberFinalized();
+  const finalizedHeadHash = await api.rpc.chain.getFinalizedHead();
+  // get head of chain, as hash or number:
+  const headNumber = await api.derive.chain.bestNumber();
+  const headHash = await api.rpc.chain.getBlockHash(headNumber);
+
+  console.log(
+    `Finalized head of chain
       - as number: ${finalizedHeadNumber}
       - with hash: ${finalizedHeadHash}`
-      )
-    console.log(
-      `Head of chain
+  );
+  console.log(
+    `Head of chain
       - as number: ${headNumber}
       - with hash: ${headHash}`
-      )
+  );
 
-    // get current issuance 
-    const issuance = await api.query.balances.totalIssuance() as Balance
-    console.log(`current issuance is: ${issuance.toNumber()}tJOY`)
-    
-    // get events in newest block:
-    const events = await api.query.system.events() as Vec<EventRecord>;
-    for (let { event } of events) {
-      const section = event.section
-      const method = event.method
-      const data = event.data
-      console.log("section",section)
-      console.log("method",method)
-      console.log("data",data.toHuman())
-      console.log("")
-    }
+  // get current issuance
+  const issuance = (await api.query.balances.totalIssuance()) as Balance;
+  console.log(`current issuance is: ${issuance.toNumber()}tJOY`);
 
-    // get extrinsics in finalized head block:
-    const getLatestBlock = await api.rpc.chain.getBlock(finalizedHeadHash) as SignedBlock
-    const extrinsics = getLatestBlock.block.extrinsics as Vec<Extrinsic>
-    for (let i=0; i<extrinsics.length; i++) {
-      const section = extrinsics[i].method.section
-      const method = extrinsics[i].method.method
-      console.log("section",section)
-      console.log("method",method)
-      console.log("")
-      // get signer of extrinsics if applicable
-      const signer = extrinsics[i].signer
-      if (!signer.isEmpty) {
-        console.log("signer",signer)
-      }
+  // get events in newest block:
+  const events = (await api.query.system.events()) as Vec<EventRecord>;
+  for (let { event } of events) {
+    const section = event.section;
+    const method = event.method;
+    const data = event.data;
+    console.log("section", section);
+    console.log("method", method);
+    console.log("data", data.toHuman());
+    console.log("");
+  }
+
+  // get events in newest block:
+  const events = (await api.query.system.events()) as Vec<EventRecord>;
+  for (let { event } of events) {
+    const section = event.section;
+    const method = event.method;
+    const data = event.data;
+    console.log("section", section);
+    console.log("method", method);
+    console.log("data", data.toHuman());
+    console.log("");
+  }
+
+  // get extrinsics in finalized head block:
+  const getLatestBlock = (await api.rpc.chain.getBlock(
+    finalizedHeadHash
+  )) as SignedBlock;
+  const extrinsics = getLatestBlock.block.extrinsics as Vec<Extrinsic>;
+  for (let i = 0; i < extrinsics.length; i++) {
+    const section = extrinsics[i].method.section;
+    const method = extrinsics[i].method.method;
+    console.log("section", section);
+    console.log("method", method);
+    console.log("");
+    // get signer of extrinsics if applicable
+    const signer = extrinsics[i].signer;
+    if (!signer.isEmpty) {
+      console.log("signer", signer);
     }
-    
-    api.disconnect()
+  }
+
+  api.disconnect();
 }
 
-main()
+main();
