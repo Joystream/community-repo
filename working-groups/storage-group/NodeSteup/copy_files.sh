e2e41a31373a0a8a49449b3dc7fc2fb82b57d41b
yasiryagi <yasir.yagi@gmail.com>
2023-10-28 09:23:43 +0100

    Create copy_files.sh

---
+++ working-groups/storage-group/NodeSteup/copy_files.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Copy files in 10 concurrent transfer
+# Needed variables below
+
+SERVER="server.name"
+USERNAME="username"
+PASSWORD="password"
+DIR="/target/to/copy/to/directory"
+FILEDIR="/local/files/directory"
+
+# Create work Directory 
+mkdir WORKDIR
+cd WORKDIR
+
+# install needed software
+apt install sshpass rsync -y
+
+#Create files with list of files to be transfered
+find $FILEDIR -maxdepth 1  -type f  > my_files.txt 
+split my_files.txt -n l/10 split_
+
+#Transfer files
+for file in split_*; do sshpass -p $PASSWORD rsync -avz -e "ssh -o StrictHostKeyChecking=no" --files-from=$file / $USERNAME@$SERVER:$DIR & done; wait
