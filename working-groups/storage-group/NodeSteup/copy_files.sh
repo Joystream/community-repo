b94c1ba9898ef355035d1f9773563f50aed4bf34
yasiryagi <yasir.yagi@gmail.com>
2023-10-30 10:12:32 +0000

    Update copy_files.sh

--- working-groups/storage-group/NodeSteup/copy_files.sh
+++ working-groups/storage-group/NodeSteup/copy_files.sh
@@ -8,18 +8,19 @@ USERNAME="username"
 PASSWORD="password"
 TARGET_DIR="/target/to/copy/to/directory"
 FILEDIR="/local/files/directory"
+WORKDIR="WORKDIR"
 
 # Create work Directory 
-mkdir WORKDIR
-cd WORKDIR
+mkdir $WORKDIR
+cd $WORKDIR
 
 # install needed software
 apt install sshpass rsync -y
 #apt install sshpass rsync pv tmux -y
 
 #Create 10 files with list of files to be transfered
-find $FILEDIR -maxdepth 1  -type f  > my_files.txt 
-split my_files.txt -n l/$NUM_PROCESSORS split_
+find $FILEDIR -maxdepth 1  -type f  > files_list.txt 
+split files_list.txt -n l/$NUM_PROCESSORS split_
 
 #Transfer files
 for file in split_*; do sshpass -p $PASSWORD rsync -avz -e "ssh -o StrictHostKeyChecking=no" --files-from=$file / $USERNAME@$SERVER:$DIR & done; wait
