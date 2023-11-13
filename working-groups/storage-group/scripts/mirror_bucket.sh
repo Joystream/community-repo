2d8ec48bbe4aaac0388ac373cdb2c85e9ef3eb8d
yasiryagi <yasir.yagi@gmail.com>
2023-11-13 19:51:26 +0000

    Update mirror_bucket.sh

--- working-groups/storage-group/scripts/mirror_bucket.sh
+++ working-groups/storage-group/scripts/mirror_bucket.sh
@@ -8,12 +8,17 @@ JOYSTREAMDIR='/root/joystream'
 SRC_BUCKET_ID="2"
 DES_BUCKET_ID="18"
 GRAPHQL="https://joystream.yyagi.cloud/graphql"
-QUERY='{"query": " { storageBuckets(where: {id_eq: \"'"$SRC_BUCKET_ID"'\"}, orderBy: createdAt_ASC) { bags { id } } }"}'
+SRC_QUERY='{"query": " { storageBuckets(where: {id_eq: \"'"$SRC_BUCKET_ID"'\"}, orderBy: createdAt_ASC) { bags { id } } }"}'
+DES_QUERY='{"query": " { storageBuckets(where: {id_eq: \"'"$DES_BUCKET_ID"'\"}, orderBy: createdAt_ASC) { bags { id } } }"}'
 PARSER='.data.storageBuckets[0].bags[].id'
 
-curl --header "Content-Type: application/json"  --request POST   --data "$QUERY"  $GRAPHQL | jq -r $PARSER > $FILEDIR/bags_bucket${SRC_BUCKET_ID}.txt
+curl --header "Content-Type: application/json"  --request POST   --data "$SRC_QUERY"  $GRAPHQL | jq -r $PARSER > $FILEDIR/bags_bucket${SRC_BUCKET_ID}.txt
+curl --header "Content-Type: application/json"  --request POST   --data "$DES_QUERY"  $GRAPHQL | jq -r $PARSER > $FILEDIR/bags_bucket${DES_BUCKET_ID}.txt
 
-split $FILEDIR/bags_bucket${SRC_BUCKET_ID}.txt -n l/$NUM_PROCESSORS $FILEDIR/split_bucket${SRC_BUCKET_ID}_
+diff $FILEDIR/bags_bucket${SRC_BUCKET_ID}.txt $FILEDIR/bags_bucket${DES_BUCKET_ID}.txt  | grep "<" | cut -d " " -f 2 > $FILEDIR/bags_bucket${SRC_BUCKET_ID}_diff_${DES_BUCKET_ID}.txt
+#cat bags_bucket${SRC_BUCKET_ID}_diff_${DES_BUCKET_ID}.txt
+
+split bags_bucket${SRC_BUCKET_ID}_diff_${DES_BUCKET_ID}.txt -n l/$NUM_PROCESSORS $FILEDIR/split_bucket${SRC_BUCKET_ID}_
 
 cd $JOYSTREAMDIR
 yarn storage-node leader:update-bucket-status -i $DES_BUCKET_ID -s on -k $KEYFILE -p $PASSWORD
@@ -27,3 +32,4 @@ do
   done
   yarn storage-node leader:update-bags -i $patch -a $DES_BUCKET_ID -k $KEYFILE -p $PASSWORD
 done
+
